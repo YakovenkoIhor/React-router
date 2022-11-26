5a86c03b8b1be4488772d1bed17e49f28e5b02a3
YakovenkoIhor <Ihor527@gmail.com>
2022-11-26 16:19:17 +0200

    modified:   src/App.css         modified:   src/hooks/useGetData.js         modified:   src/routes/Posts/component.jsx         new file:   src/routes/Posts/styles.scss

--- src/routes/Posts/component.jsx
+++ src/routes/Posts/component.jsx
@@ -1,72 +1,102 @@
-// import { Formik, Form, Field, ErrorMessage} from 'formik';
 import React, { useState } from 'react';
+
+import './styles.scss'
 
 import { useFormik } from 'formik';
 
-// import { useEffect, useState } from "react";
+import * as Yup from 'yup';
 
 const Posts = () => {
+  const [posts, setPosts] = useState([])
   const formik = useFormik({
-
+      
       initialValues: {
         title: '',
-        textarea : '',
-        body: '',
+        body : '',
+        userId: '',
       },
 
+      validationSchema: Yup.object({
+        title: Yup.string()
+          .max(15, 'Must be 15 characters or less')
+          .required('Required'),
+        body: Yup.string()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required'),
+        userId: Yup.string()
+          .required('Required'),
+        // email: Yup.string().email('Invalid email address').required('Required'),
+      }),
+
       onSubmit: values => {
-        // alert(JSON.stringify(values, null, 2));
-      // const [posts, setPosts] = useState([])
-        
-        const tmp = fetch('https://jsonplaceholder.typicode.com/posts', {
+          
+        fetch('https://jsonplaceholder.typicode.com/posts', {
           method: 'POST',
           body: JSON.stringify({
             title: values.title,
             body: values.body,
-            textarea: values.textarea,
-            // userId: 1,
+            userId: 1,
           }),
           headers: {
             'Content-type': 'application/json; charset=UTF-8',
           },
         })
           .then((response) => response.json())
-          .then((post) => console.log(post));
+          .then(post => {setPosts([...posts, post])}
+          );
+
       },
     });
-    console.log(formik);
 
     return (
-      <form onSubmit={formik.handleSubmit}>
-
-        <label htmlFor="title">Title</label>
-        <input
+      <>
+      <form onSubmit={formik.handleSubmit} >
+<div>
+        <label htmlFor="title" >Title</label>
+        <input 
           id="title"
           name="title"
           type="text"
           onChange={formik.handleChange}
           value={formik.values.title}
         />
-
-        <label htmlFor="textarea">Textarea</label>
-        <input
-          id="textarea"
-          name="textarea"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.textarea}
-        />
-
+</div>
+<div>
         <label htmlFor="body">Body</label>
-        <input
+        <textarea
           id="body"
           name="body"
           type="text"
           onChange={formik.handleChange}
-          value={formik.values.body}
+          value={formik.values.textarea}
         />
+</div>
+<div>
+        <label htmlFor="userId">UserId</label>
+        <select 
+          id="userId"
+          name="userId"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.body}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        </div>
         <button type="submit">Submit</button>
       </form>
+      {posts.length === 0 ? <p>No add posts</p> : posts.map(post => {
+        return (
+          <span>
+            Title: {post.title} <br />
+            Body: {post.body} 
+            </span>
+        )
+      })}
+      </>
     );
+    
     }
   export default Posts
